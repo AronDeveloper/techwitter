21d03f25ba446359bd65f7d5bdea5587add4dc10
AronDeveloper <aronisufi2608@gmail.com>
2021-12-24 19:22:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ghyg6x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ghyg6x)
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <div className="navbar">
+        <nav>
+          <ul>
+            <strong>Techwitter</strong>
+            <li>
+              <a href="techtoks.js">Hi</a>
+            </li>
+            <li>
+              <a href="techtoks.js">hello</a>
+            </li>
+            <li>
+              <a href="techtoks.js">Hello friend</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="body">
+        <h2>See techwitters</h2>
+      </div>
+    </>
+  );
+}
